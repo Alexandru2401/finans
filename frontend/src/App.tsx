bb8495992c4116dbfa1b6844bfc836c31f9d5d9d
Alexandru2401 <a.toma231@yahoo.com>
2026-02-08 19:41:33 +0200

    install shadcn and tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <p>Test page</p>
-      <p>new test page</p>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
