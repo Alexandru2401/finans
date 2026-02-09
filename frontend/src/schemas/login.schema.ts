c32419f53c836ab775211e67f52d5630560ee4e8
Alexandru2401 <a.toma231@yahoo.com>
2026-02-09 09:06:59 +0200

    add zod and a basic schema and validations for login page

---
+++ frontend/src/schemas/login.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const LoginSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required." })
+    .email("Please enter a valid email."),
+
+  password: z.string().min(1, { message: "Password is required." }),
+});
+
+export default LoginSchema;
