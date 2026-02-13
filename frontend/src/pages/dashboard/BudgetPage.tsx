2ed60fe17d3b9b44e6406a8fd7da2d47770c2e2d
Alexandru2401 <a.toma231@yahoo.com>
2026-02-13 16:27:32 +0200

    feat(public): add scroll progress bar in main layout

--- frontend/src/pages/dashboard/BudgetPage.tsx
+++ frontend/src/pages/dashboard/BudgetPage.tsx
@@ -1,3 +1,32 @@
+import {
+  TrendingDown,
+  TrendingUp,
+  Wallet,
+  CreditCard,
+  DollarSign,
+  AlertCircle,
+} from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+
 export default function BudgetPage() {
-  return <div>Budget Page</div>;
+  return (
+    <section className="py-8 px-4 md:px-8 max-w-7xl mx-auto">
+      <h1 className="text-2xl font-bold mb-6">Budget Overview</h1>
+
+      <div className="grid grid-cols-2 gap-4">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    </section>
+  );
 }
--- frontend/src/routes/layout/PublicRootLayout.tsx
+++ frontend/src/routes/layout/PublicRootLayout.tsx
@@ -1,14 +1,43 @@
 import { Outlet } from "react-router";
+import { useEffect, useState } from "react";
 import PublicMainNavigation from "../navigation/PublicMainNavigation";
 import PublicFooter from "@/components/public/Footer";
 
 export default function PublicRootLayout() {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const progress = (scrollTop / docHeight) * 100;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       <PublicMainNavigation />
-      <main className="max-w-250 flex justify-center mx-auto flex-col">
+
+      <main className="relative max-w-250 flex justify-center mx-auto flex-col border-4 border-y border-gray-100 ">
+        <div
+          className="absolute top-0 -left-1 w-1 bg-yellow-300 transition-all duration-150 z-50"
+          style={{ height: `${scrollProgress}%` }}
+        />
+
         <Outlet />
+        <div
+          className="absolute top-0 -right-1 w-1 bg-yellow-300 transition-all duration-150 z-50"
+          style={{ height: `${scrollProgress}%` }}
+        />
       </main>
+
       <PublicFooter />
     </>
   );
